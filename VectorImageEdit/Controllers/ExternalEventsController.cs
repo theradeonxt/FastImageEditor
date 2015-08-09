95e4fbc76d5c4167a5679f0b64fed42a8956e604
theradeonxt <theradeonxt@gmail.com>
2015-08-09 16:07:10 +0300

    missing file

---
+++ VectorImageEdit/Controllers/ExternalEventsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using VectorImageEdit.Forms;
+using VectorImageEdit.Forms.AppWindow;
+using VectorImageEdit.Models;
+
+namespace VectorImageEdit.Controllers
+{
+    class ExternalEventsController
+    {
+        private AppWindow _appView;
+        private ExternalEventsModel _model;
+
+        ExternalEventsController(AppWindow appView, ExternalEventsModel model)
+        {
+            _appView = appView;
+            _model = model;
+
+            _appView.AddSaveVectorListener(new SaveVectorListener());
+        }
+
+        private class SaveVectorListener: IActionListener
+        {
+            SaveVectorListener()
+            public void ActionPerformed(object sender, EventArgs e)
+            {
+                
+            }
+        }
+    }
+}
