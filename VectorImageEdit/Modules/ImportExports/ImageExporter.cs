1054eaffef61a5ccf4f6cfd3de484ea6aaefd0aa
theradeonxt <theradeonxt@gmail.com>
2015-10-08 22:45:14 +0300

    Some missing files got lost

---
+++ VectorImageEdit/Modules/ImportExports/ImageExporter.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace VectorImageEdit.Modules.ImportExports
+{
+    class ImageExporter : AbstractExporter<ImageFormat, Bitmap>
+    {
+        public ImageExporter(string fileName)
+            : base(fileName)
+        {
+            DataSource = Properties.Resources.placeholder;
+            ExportParameter = ImageFormat.Jpeg;
+        }
+
+        public override bool ExportData()
+        {
+            bool status = ExportValidator(() =>
+            {
+                if (ExportParameter.Guid == ImageFormat.Jpeg.Guid)
+                {
+                    // For JPEG format, set quality to 90 (seems to have best quality & size balance)
+                    Encoder myEncoder = Encoder.Quality;
+                    EncoderParameters myEncoderParameters = new EncoderParameters(1);
+                    EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 90L);
+                    myEncoderParameters.Param[0] = myEncoderParameter;
+                    
+                    ImageCodecInfo jpegCodecInfo = ImageCodecInfo.GetImageDecoders()
+                        .First(codec => codec.FormatID == ExportParameter.Guid);
+
+                    DataSource.Save(FileName, jpegCodecInfo, myEncoderParameters);
+                }
+                else
+                {
+                    // Other image codecs are handled in the same way
+                    DataSource.Save(FileName, ExportParameter);
+                }
+            });
+            return status;
+        }
+    }
+}
